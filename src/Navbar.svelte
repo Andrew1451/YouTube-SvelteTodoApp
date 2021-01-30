9102dc2edcf01f4d5660fe7832e335390bd09a3b
Andrew <andrew.gaynor@ymail.com>
2021-01-30 16:53:02 -0500

    add navbar

---
+++ src/Navbar.svelte
@@ -0,0 +1,41 @@
+<nav>
+    <a id='logo' href='/'>TechCheck</a>
+    <ul>
+        <li><a href='/'>Home</a></li>
+        <li><a href='/not-home'>Not Home</a></li>
+    </ul>
+</nav>
+
+<style>
+    nav {
+        height: 57px;
+        width: 100%;
+        margin: 0;
+        margin-bottom: 55px;
+        background-color: #22828c;
+
+    }
+    #logo {
+        color: white;
+        font-size: 2.3rem;
+        padding-left: 38px;
+        display: inline-block;
+        padding-top: 5px;
+    }
+    ul {
+        height: 57px;
+        margin: 0;
+        display: inline-flex;
+        justify-content: space-around;
+        float: right;
+        list-style-type: none;
+        width: 25%;
+        align-items: center;
+        margin-right: 35px;
+    }
+    a {
+        color: white;
+        text-decoration: none;
+        font-size: 25px;
+    }
+</style>
