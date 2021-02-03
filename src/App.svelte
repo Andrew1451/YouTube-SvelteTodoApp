5f035efe978cbb7bb12eeb4a3eae851b55cbd171
Andrew <andrew.gaynor@ymail.com>
2021-02-03 18:33:20 -0500

    add functionality of todo list

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,28 @@
 <script>
 	import Navbar from './Navbar.svelte';
+
+	let todo = '';
+	let todos = [];
+
+	const submitTodo = () => {
+		todos = [...todos, todo];
+		todo = '';
+	}
 </script>
 
 <Navbar />
 <main>
-
+	<form>
+		<input type='text' required bind:value={todo} />
+		<button type='submit' on:click|preventDefault={submitTodo}>Submit</button>
+	</form>
+	{#if todos.length === 0}
+		<p>Add todos!</p>
+	{:else}
+		{#each todos as todo}
+			<p>{todo}</p>
+		{/each}
+	{/if}
 </main>
 
 <style>
@@ -13,6 +31,10 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
+	}
+
+	input:focus {
+		outline: none;
 	}
 
 	@media (min-width: 640px) {
